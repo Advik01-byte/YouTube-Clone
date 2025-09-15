77507df89594c9e19629538269a96253320182ad
Advik01-byte <iamadvikgoyal@gmail.com>
2025-09-15 16:25:44 +0530

    Add files via upload

---
+++ data/videos.js
@@ -0,0 +1,134 @@
+export const videos = [
+  {
+    title: 'Talking Tech and AI with Google CEO Sundar Pichai!',
+    author: 'Marques Brownlee',
+    stats: '3.4M views &middot; 6 months ago',
+    videoTime: '14:20',
+    images: {
+      channel: 'images/channel-pictures/channel-1.jpeg',
+      thumbnail: 'images/thumbnails/thumbnail-1.webp'
+    },
+    link: 'https://www.youtube.com/watch?v=n2RNcPRtAiY'
+  },
+  {
+    title: 'Try Not To Laugh Challenge &#x23;9',
+    author: 'Markiplier',
+    stats: '19M views &middot; 4 years ago',
+    videoTime: '8:22',
+    images: {
+      channel: 'images/channel-pictures/channel-2.jpeg',
+      thumbnail: 'images/thumbnails/thumbnail-2.webp'
+    },
+    link: 'https://www.youtube.com/watch?v=mP0RAo9SKZk'
+  },
+  {
+    title: 'Crazy Tik Toks Taken Moments Before DISASTER',
+    author: 'SSSniperWolf',
+    stats: '12M views &middot; 1 year ago',
+    videoTime: '9:13',
+    images: {
+      channel: 'images/channel-pictures/channel-3.jpeg',
+      thumbnail: 'images/thumbnails/thumbnail-3.webp'
+    },
+    link: 'https://www.youtube.com/watch?v=FgjPQQeTh1w'
+  },
+  {
+    title: 'The Simplest Math Problem No One Can Solve - Collatz Conjecture',
+    author: 'Veritasium',
+    stats: '18M views &middot; 4 months ago',
+    videoTime: '22:09',
+    images: {
+      channel: 'images/channel-pictures/channel-4.jpeg',
+      thumbnail: 'images/thumbnails/thumbnail-4.webp'
+    },
+    link: 'https://www.youtube.com/watch?v=094y1Z2wpJg'
+  },
+  {
+    title: 'Kadane\'s Algorithm to Maximum Sum Subarray Problem',
+    author: 'CS Dojo',
+    stats: '519K views &middot; 5 years ago',
+    videoTime: '11:17',
+    images: {
+      channel: 'images/channel-pictures/channel-5.jpeg',
+      thumbnail: 'images/thumbnails/thumbnail-5.webp'
+    },
+    link: 'https://www.youtube.com/watch?v=86CQq3pKSUw'
+  },
+  {
+    title: 'Anything You Can Fit In The Circle I\'ll Pay For',
+    author: 'MrBeast',
+    stats: '141M views &middot; 1 year ago',
+    videoTime: '19:59',
+    images: {
+      channel: 'images/channel-pictures/channel-6.jpeg',
+      thumbnail: 'images/thumbnails/thumbnail-6.webp'
+    },
+    link: 'https://www.youtube.com/watch?v=yXWw0_UfSFg'
+  },
+  {
+    title: 'Why Planes Don\'t Fly Over Tibet',
+    author: 'RealLifeLore',
+    stats: '6.6M views &middot; 1 year ago',
+    videoTime: '10:13',
+    images: {
+      channel: 'images/channel-pictures/channel-7.jpeg',
+      thumbnail: 'images/thumbnails/thumbnail-7.webp'
+    },
+    link: 'https://www.youtube.com/watch?v=fNVa1qMbF9Y'
+  },
+  {
+    title: 'Inside The World\'s Biggest Passenger Plane',
+    author: 'Tech Vision',
+    stats: '3.7M views &middot; 10 months ago',
+    videoTime: '7:12',
+    images: {
+      channel: 'images/channel-pictures/channel-8.jpeg',
+      thumbnail: 'images/thumbnails/thumbnail-8.webp'
+    },
+    link: 'https://www.youtube.com/watch?v=lFm4EM1juls'
+  },
+  {
+    title: 'The SECRET to Super Human STRENGTH',
+    author: 'ThenX',
+    stats: '20M views &middot; 3 years ago',
+    videoTime: '13:17',
+    images: {
+      channel: 'images/channel-pictures/channel-9.jpeg',
+      thumbnail: 'images/thumbnails/thumbnail-9.webp'
+    },
+    link: 'https://www.youtube.com/watch?v=ixmxOlcrlUc'
+  },
+  {
+    title: 'How The World\'s Largest Cruise Ship Makes 30,000 Meals Every Day',
+    author: 'Business Insider',
+    stats: '14M views &middot; 1 year ago',
+    videoTime: '7:53',
+    images: {
+      channel: 'images/channel-pictures/channel-10.jpeg',
+      thumbnail: 'images/thumbnails/thumbnail-10.webp'
+    },
+    link: 'https://www.youtube.com/watch?v=R2vXbFp5C9o'
+  },
+  {
+    title: 'Dubai\'s Crazy Underwater Train and Other Things &#x23;Only in Dubai',
+    author: 'Destination Tips',
+    stats: '3M views &middot; 1 year ago',
+    videoTime: '4:10',
+    images: {
+      channel: 'images/channel-pictures/channel-11.jpeg',
+      thumbnail: 'images/thumbnails/thumbnail-11.webp'
+    },
+    link: 'https://www.youtube.com/watch?v=0nZuYyXET3s'
+  },
+  {
+    title: 'What would happen if you didn\'t drink water? - Mai Nacamulli',
+    author: 'TED-Ed',
+    stats: '12M views &middot; 5 years ago',
+    videoTime: '4:51',
+    images: {
+      channel: 'images/channel-pictures/channel-12.jpeg',
+      thumbnail: 'images/thumbnails/thumbnail-12.webp'
+    },
+    link: 'https://www.youtube.com/watch?v=9iMGFqMmUFs'
+  }
+];
---
+++ scripts/youtube-clone.js
@@ -0,0 +1,43 @@
+import { videos } from '../data/videos.js';
+
+/*
+  Main idea of JavaScript:
+    1. Save the data {Done in the videos.js file in the data folder}
+    2. Generate the HTML
+    3. Make it interactive (not following this time)
+*/
+
+let videosHTML = '';
+
+videos.forEach((video) => {
+  videosHTML += `
+    <a href="${video.link}" target="_blank" class="video-link">
+      <div class="video-preview">
+        <div class="thumbnail-row">
+          <img src="${video.images.thumbnail}" class="css-thumbnail">
+          <div class="video-time">
+            ${video.videoTime}
+          </div>
+        </div>
+        <div class="video-info-grid">
+          <div class="css-channel-picture">
+            <img src="${video.images.channel}" class="css-profile-picture">
+          </div>
+          <div class="css-video-info">
+            <p class="css-video-title">
+              ${video.title}
+            </p>
+            <p class="css-video-author">
+              ${video.author}
+            </p>
+            <p class="css-video-stats">
+              ${video.stats}
+            </p>
+          </div>
+        </div>
+      </div>
+    </a>
+  `;
+});
+
+document.querySelector('.js-video-grid').innerHTML = videosHTML;
